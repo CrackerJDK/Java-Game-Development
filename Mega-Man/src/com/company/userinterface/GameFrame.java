a749604a7e535da0a6fc2fecacf9dd115b2c76dc
Hải Đăng JDK <luongvbhp1212@gmail.com>
2021-11-10 05:07:57 +0700

    Initial commit

---
+++ Mega-Man/src/com/company/userinterface/GameFrame.java
@@ -0,0 +1,32 @@
+package com.company.userinterface;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame{
+
+    public static final int SCREEN_WIDTH = 1000;
+    public static final int SCREEN_HEIGHT = 600;
+
+    public GameFrame(){
+        /**
+         * call getToolKit() method inside abstract class Toolkit
+         */
+        Toolkit toolkit = this.getToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        this.setBounds((dimension.width-SCREEN_WIDTH)/2,(dimension.height-SCREEN_HEIGHT)/2,SCREEN_WIDTH,SCREEN_HEIGHT);
+
+        /**
+         * USE this TO CALL setDefaultCloseOperation() method belong to JFrame extends by GameFrame class
+         * SET CLOSE BUTTON TO STOP PROGRAM
+         */
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        GameFrame gameFrame = new GameFrame();
+        gameFrame.setVisible(true);
+    }
+
+
+}
